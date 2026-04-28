9a9ed4def0481d21dac354db1d801bab49917c82
kebrtq <ibrahim.ahmedkeb1@gmail.com>
2026-04-29 02:52:16 +0300

    fix search suspense properly

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,8 @@
 ﻿import { Suspense } from "react"
 import { SearchResults } from "@/components/search-results"
 
+export const dynamic = 'force-dynamic'
+
 export default function SearchPage() {
   return (
     <div className="container mx-auto px-4 py-8">
