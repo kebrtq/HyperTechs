5a6edcf9b3297a1601bf486a018634683bfbc092
kebrtq <ibrahim.ahmedkeb1@gmail.com>
2026-04-30 02:08:23 +0300

    use next sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,7 @@
 export default function sitemap() {
   return [
-    {
-      url: 'https://hypertechiq.com',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://hypertechiq.com/cart',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://hypertechiq.com/checkout',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://hypertechiq.com/search',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://hypertechiq.com/warranty',
-      lastModified: new Date(),
-    },
+    { url: 'https://hypertechiq.com' },
+    { url: 'https://hypertechiq.com/products' },
+    { url: 'https://hypertechiq.com/contact' },
   ]
 }
