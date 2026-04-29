7b5dec6a58e7a07e995a213c4caa7b5e65bde1dd
kebrtq <ibrahim.ahmedkeb1@gmail.com>
2026-04-29 06:10:45 +0300

    remove all edge runtime

--- app/api/send-order/route.ts
+++ app/api/send-order/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
-export const runtime = 'edge'
-
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: NextRequest) {
